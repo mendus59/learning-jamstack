218281a3d6084c311397b92849cbceceae311751
Patrick Mendus <mendus59@gmail.com>
2020-09-18 20:47:47 -0700

    Added localization and README

---
+++ README.md
@@ -0,0 +1,30 @@
+# Learning the JAMStack
+
+This project was created while following the JAMStack Tutorial https://www.youtube.com/watch?v=A_l0qrPUJds
+
+## Installation
+
+This project deploys with as a Continuous Integration with NPM, GitHub, and Netlify
+
+To grab all necessary dependencies, run:
+```
+npm build
+```
+
+## Usage
+```
+npm start
+```
+
+## Stack and Technologies
+### JAMStack
+Javascript: Eleventy (chosen with the help of https://www.staticgen.com/), npm, axios, dotenv
+APIs: https://newsapi.org/
+Markup: Nunjucks
+
+### Deployment
+GitHub
+Netlify
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,26 @@
 const axios = require("axios");
+const countries = require("./countries.json");
 require('dotenv').config();
-//${process.env.NEWS_API_KEY}
-module.exports = async function() {
+
+async function getNews(country) {
     try {
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
-        return response.data;
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}`);
+        return {
+            "country": country,
+            "articles": response.data.articles
+        }
     } catch (error){
         console.error(error);
     }
+}
+
+
+module.exports = async function() {
+    
+    var newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then(newsObjects => {
+        console.log('newsObjects: ', newsObjects);
+        return [].concat.apply([], newsObjects);
+    });
+
 }
